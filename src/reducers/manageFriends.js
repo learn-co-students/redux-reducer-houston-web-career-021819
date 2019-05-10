532f604d7a3db194cd8be65b79e74ee713328089
HemalathaMurugan <hemalatha.psna@gmail.com>
2019-05-09 22:58:25 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,41 @@
-export function manageFriends(state, action){
+
+export function manageFriends(state = {friends}, action){
+   switch(action.type){
+       case 'ADD_FRIEND':
+           return {...state,
+            friends: [...state.friends, action.friend]
+            } 
+            
+     
+       case 'REMOVE_FRIEND':
+            return {...state, friends: state.friends.filter(friend => friend.id!==action.id)}
+      
+       default: 
+            return state
+       
+   } 
+   
 }
+
+
+// export function manageFriends(state = {friends}, action){
+//     switch(action.type){
+//         case 'ADD_FRIEND':
+//             return {...state,
+//              friends: [...state.friends, action.friend]
+//thisline when I had -> friends: [...state.friends, action.friend]-> threw error
+//              } 
+             
+      
+//         case 'REMOVE_FRIEND':
+//              return {...state, friends: state.friends.filter(friend => friend.id!==action.id)}
+       
+//         default: 
+//              return state
+        
+//     } 
+    
+//  }
+//{...state.friends, state.friends.filter(friend => friend.id!== action.friend.id)
+//threw errors ->state.friends -> dont have. action.id -> correct ;action.friend.id -> not correct}
+ 
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,20 @@
-export function managePresents(state, action){
+let numberOfPresents = 0
+
+
+export function managePresents(state = { numberOfPresents: 0}, action){
+    switch(action.type){
+        case 'INCREASE':
+            state = {...state, numberOfPresents: numberOfPresents+1}
+        break;
+           
+        case (!'INCREASE'):
+            state = state
+        break;
+        
+    
+    }
+    
+    return state
+   
+    //  return {...state, numberOfPresents: numberOfPresents+1}
 }
